fee2a1d8b44a734f11f95ceee2fc6b62e76153fc
Dan <dantakayosh@gmail.com>
2015-10-29 14:05:13 -0400

    added styling and fixes to userPage

--- client/app/main/main.controller.js
+++ client/app/main/main.controller.js
@@ -2,35 +2,23 @@
 
 angular.module('pinterestApp')
   .controller('MainCtrl', function ($scope, $http, $location, $rootScope, Auth) {
-
+  
   $scope.isCollapsed = true;
   $scope.isLoggedIn = Auth.isLoggedIn;
   $scope.isAdmin = Auth.isAdmin;
   $scope.getCurrentUser = Auth.getCurrentUser;    
-
-  $scope.usersPinsArray = [];
-
-  $http.get('/api/pins/').success(function(response) { 
-    // console.log(response);
-    $scope.allPins = response;
-  });
 
   $scope.goToUserPage = function(pin) {
     $rootScope.user_id = pin.author_id;
     $rootScope.author_name = pin.author_name;
     $location.path('/userpage/' + pin.author_name);
     console.log("page loaded");
-    test();
-  };
-
-  var test = function() {
-    $scope.showAllPins = true;
-    $scope.getUsersPins();
   };
 
   $scope.getUsersPins = function() {
     $http.get('/api/pins/userPins/' + $rootScope.user_id).success(function(response) {
       console.log(response);
+      $scope.showAllPins = true;
       $scope.allUsersPins = response;
     });
   };
@@ -46,5 +34,16 @@ angular.module('pinterestApp')
     $scope.collection = collection;
     $scope.showPins = true;
   };    
+
+  if ($location.$$path === '/') {
+    $http.get('/api/pins/').success(function(response) { 
+      // console.log(response);
+      $scope.allPins = response;
+    });
+  } 
+  else if ($location.$$path.substr(1, 8) === 'userpage') {
+    $scope.getUsersPins();
+  }
+  console.log($location.$$path);  
 
 });
--- client/app/mypins/mypins.controller.js
+++ client/app/mypins/mypins.controller.js
@@ -16,6 +16,8 @@ angular.module('pinterestApp')
 
     $http.post('/api/pins/', newPin).success(function(response) {
       console.log(response);
+      $scope.newPin = null;
+      $scope.getUsersPins();
     });
   };
 
@@ -66,6 +68,9 @@ angular.module('pinterestApp')
       $scope.allUsersPins = response;
     });
   };
+  // fetch pins when page is loaded
+  $scope.getUsersPins();
+  $scope.showAllPins = true;
 
   $scope.getUsersCollections = function() {
     $http.get('/api/pincollections/usersCollections/' + $scope.getCurrentUser()._id).success(function(response) {
@@ -74,13 +79,15 @@ angular.module('pinterestApp')
     });  
   };
   // fetch collections when page is loaded
-  $scope.getUsersCollections();
-  $scope.showCollections = true;
+  // $scope.getUsersCollections();
+  // $scope.showCollections = true;
 
   $scope.showAddToCollection = function(collection, index) {
     $scope.collectionToEdit = collection;
     $scope.collectionToEditIndex = index;
     $scope.showAddPinToCollectionBox = true;
+    var collectionArea = document.getElementById('collection-area');
+    collectionArea.style.opacity = 0.2;
   };
 
   $scope.addToCollection = function(newPin) {
@@ -98,7 +105,8 @@ angular.module('pinterestApp')
 
         // TODO: this call to the db is unnecessary, fix this simple solution later. 
         $scope.getUsersCollections();
-
+        var collectionArea = document.getElementById('collection-area');
+        collectionArea.style.opacity = 1.0;        
         $scope.newPin = null;     
       })
     });
@@ -107,11 +115,19 @@ angular.module('pinterestApp')
   $scope.showPinsInCollection = function(collection) {
     $scope.collection = collection;
     $scope.showPins = true;
+    var collectionsArea = document.getElementById('collection-area');
+    collectionsArea.style.opacity = 0.4;
   };  
 
+  $scope.leavePinsInCollectionArea = function() {
+    $scope.showPins = false;
+    var collectionsArea = document.getElementById('collection-area');
+    collectionsArea.style.opacity = 1.0;
+  }; 
+
   $scope.editPin = function(selectedPin, collection, index) {
-    var test = document.getElementById('wrapper');
-    test.style.opacity = 0.4;
+    var showAllPinsArea = document.getElementById('show-all-pins-area');
+    showAllPinsArea.style.opacity = 0.4;
     $scope.showPinEdit  = true;
     $scope.pinToEdit = selectedPin;
     $scope.collectionToEdit = collection;
@@ -122,15 +138,14 @@ angular.module('pinterestApp')
   };  
 
   $scope.submitEdit = function(updatedPin) {
-
     var updatePinInCollection = function(updatedPin, collection, index) {
       $http.put('/api/pins/' + updatedPin._id, updatedPin).success(function(response) {
         console.log(response);
         $http.put('/api/pinCollections/' + collection._id, collection).success(function(response) {
           console.log(response);
           $scope.collection = response;
-          var test = document.getElementById('wrapper');
-          test.style.opacity = 1.0;
+          var showAllPinsArea = document.getElementById('show-all-pins-area');
+          showAllPinsArea.style.opacity = 1.0;
           $scope.showPinEdit = false;
 
           // Lets variables be garbage collected faster to prevent mem leaks?
@@ -140,7 +155,7 @@ angular.module('pinterestApp')
           $scope.pinToEdit = null; 
         });
       });  
-    }    
+    };    
 
     // collection and index were passed in to $scope.editPin
     if ($scope.collectionToEdit !== undefined || $scope.pinToEditIndex !== undefined) { 
@@ -167,11 +182,70 @@ angular.module('pinterestApp')
       // the pin doesn't belong to a collection, we just change the pin itself.
       $http.put('/api/pins/' + updatedPin._id, updatedPin).success(function(response) {
         console.log(response);
-        var test = document.getElementById('wrapper');
-        test.style.opacity = 1.0;
+        var showAllPinsArea = document.getElementById('show-all-pins-area');
+        showAllPinsArea.style.opacity = 1.0;
         $scope.showPinEdit = false;        
       });
     }
+  };  
+
+  $scope.deletePin = function(pin) {
+    var updatePinInCollection = function(pinToDelete, collection, index) {
+      $scope.collectionToEdit.pins.splice($scope.pinToEditIndex, 1);
+
+      // update removed pin from collection
+      $http.put('/api/pinCollections/' + $scope.collectionToEdit._id, $scope.collectionToEdit).success(function(response) {
+        console.log(response);
+        // once complete, delete the pin from the database
+        $http.delete('/api/pins/' + pinToDelete._id).then(function() {
+          $scope.showDeleteBox = false;
+          $scope.showPinEdit = false;
+
+          $scope.getUsersPins();  
+          var editBox = document.getElementById('pin-edit-box-delete');
+          editBox.style.opacity = 1.0;
+          var showAllPinsArea = document.getElementById('show-all-pins-area');
+          showAllPinsArea.style.opacity = 1.0;          
+
+          // Lets variables be garbage collected faster to prevent mem leaks?
+          $scope.pinToEdit = null; 
+          $scope.collectionToEdit = null;
+          $scope.pinToEditIndex = null;   
+          $scope.pinToEdit = null;                 
+        });
+      });
+    };
+
+    if ($scope.collectionToEdit !== undefined) {
+      updatePinInCollection(pin, $scope.collectionToEdit, $scope.collectionToEditIndex);
+    } 
+    else if (pin.collection_id !== undefined) {
+      // the pin is in an existing collection. modify both the pin and collection
+      // first we need to get the collection then find the index of the pin
+      $http.get('/api/pinCollections/' + pin.collection_id).success(function(response) {
+        for(var i = 0; i < response.pins.length; i++) {
+          if (response.pins[i]._id === pin._id) {
+            $scope.pinToEditIndex = i;
+            $scope.collectionToEdit = response;
+            $scope.collectionToEdit.pins[$scope.pinToEditIndex] = pin;
+            break;
+          } 
+        }
+        updatePinInCollection(pin, $scope.collectionToEdit, $scope.pinToEditIndex);
+      });
+    }
+    else if (pin.collection_id === undefined) {
+      // the pin doesn't belong to a collection, we just change the pin itself.
+      $http.delete('/api/pins/' + pin._id, pin).success(function(response) {
+        $scope.getUsersPins();
+        var editBox = document.getElementById('pin-edit-box-delete');
+        editBox.style.opacity = 1.0;      
+        var showAllPinsArea = document.getElementById('show-all-pins-area');
+        showAllPinsArea.style.opacity = 1.0;          
+
+        $scope.showPinEdit = false;        
+      });
+    }        
   };  
 
   $scope.cancelEdit = function() {
@@ -187,41 +261,34 @@ angular.module('pinterestApp')
 
     $scope.showPinEdit = false;
     console.log($scope.usersPins);
-    var test = document.getElementById('wrapper');
-    test.style.opacity = 1.0;
-  };
-
-  $scope.deletePin = function(pin) {
-    console.log(pin);
-    // variables saved from when edit is clicked
-    // $scope.pinToEdit = selectedPin;
-    // $scope.collectionToEdit = collection;
-    // $scope.pinToEditIndex = index;
-
-    $scope.collectionToEdit.pins.splice($scope.pinToEditIndex, 1);
-
-    // update removed pin from collection
-    $http.put('/api/pinCollections/' + $scope.collectionToEdit._id, $scope.collectionToEdit).success(function(response) {
-      console.log(response);
-      // once complete, delete the pin from the database
-      $http.delete('/api/pins/' + pin._id).then(function() {
-        $scope.showDeleteBox = false;
-        $scope.showPinEdit = false;
-        var test = document.getElementById('wrapper');
-        test.style.opacity = 1.0;
-        $scope.getUsersPins();        
-      });
-    });
+    var showAllPinsArea = document.getElementById('show-all-pins-area');
+    showAllPinsArea.style.opacity = 1.0;
   };   
 
   $scope.deleteBtnPressed = function() { 
     $scope.showDeleteBox = true;
     var editBox = document.getElementById('pin-edit-box-delete');
-    editBox.style.opacity = 0.3;
+    editBox.style.opacity = 0.0;
 
-    var deleteBoxArea = document.getElementById('del-box-area');
-    deleteBoxArea.style.margin = '0 auto';
+    // var deleteBoxArea = document.getElementById('del-box-area');
+    // deleteBoxArea.style.marginLeft = "150px";
+    // deleteBoxArea.style.marginTop = "65px";
   };
 
+  $scope.cancelDeleteBtnPressed = function () {
+    $scope.showDeleteBox = false;
+    var editBox = document.getElementById('pin-edit-box-delete');
+    editBox.style.opacity = 1.0;
+
+    // var deleteBoxArea = document.getElementById('del-box-area');
+    // deleteBoxArea.style.marginLeft = "0px";
+    // deleteBoxArea.style.marginTop = "0px";
+  };
+
+  $scope.cancelAddPinToCollection = function() {
+    $scope.showAddPinToCollectionBox = false;
+    var collectionArea = document.getElementById('collection-area');
+    collectionArea.style.opacity = 1.0;
+  };
 
   });
